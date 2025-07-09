777d11e9d805c273945f00d51115154b1ef85d30
Deepraj Mukhopadhyay <143916300+Deepraj-Mukhopadhyay@users.noreply.github.com>
2025-07-09 15:29:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+<script>
+  setTimeout(() => {
+    window.location.href = "login.html";
+  }, 3000);
+</script>
 function signInWithGoogle() {
   alert("Google Sign-In not implemented yet.\nThis is a placeholder for future Firebase Auth.");
 }
